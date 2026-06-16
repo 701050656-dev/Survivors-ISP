8e009cbac6048497b26266ba6bc77848049c9cee
701050656-dev <701050656@gotvdsb.ca>
2026-06-16 00:34:27 -0400

    Update ControlScreen.java

--- ControlScreen.java
+++ ControlScreen.java
@@ -8,41 +8,22 @@
 ************************************************
 */
 import java.awt.*;
-
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
-
 import java.awt.event.WindowAdapter;
-
 import java.awt.event.WindowEvent;
-
-
-
 import javax.swing.*;
-
-
-
 public class ControlScreen {
 
-
-
 	public static void main(String[] args) {
-
 		ControlFrame frame = new ControlFrame();
-
 		frame.addWindowListener
-
 		(new WindowAdapter()
-
 				{
-
 			public void windowClosing(WindowEvent e)
 
 			{
-
 				System.exit(0);
-
 			}
 
 				}
@@ -56,47 +37,26 @@ public class ControlScreen {
 
 
 class ControlFrame extends JFrame {
-
-	
-
 	public ControlFrame() {
-
 		super("Controls Screen");
-
-
-
 		// Main pane
-
 		JLayeredPane pane = new JLayeredPane();
-
 		pane.setLayout(null); 
 
 		
 
 		// Import Controls screen
-
-		// chatgpt
-
+// chatgpt
 		ImageIcon icon = new ImageIcon("Assets/Controls.png");
-
 		Image img = icon.getImage();
-
 		Image scaled = img.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
-
 		icon = new ImageIcon(scaled);
-
 		JLabel imageLabel = new JLabel(icon);
-
 		imageLabel.setBounds(0, 0, 800, 600);
-
 		imageLabel.setHorizontalAlignment(JLabel.CENTER);
-
 		// ends here
 
-		
-
 		// Continue button
-
 		JButton StartButton = new JButton("Continue");
 		StartButton.setBounds(325, 500, 150, 40);
 		StartButton.setBackground(Color.BLACK);
@@ -115,18 +75,14 @@ class ControlFrame extends JFrame {
 
 
 		pane.add(imageLabel, Integer.valueOf(0)); // background
-
 		pane.add(StartButton, Integer.valueOf(1)); // on top
 
 
 
 		setSize(800, 600);
-
 		setContentPane(pane);
-
 		setVisible(true);
 		setLocationRelativeTo(null); // center the frame
-		
 		
 
 	}
